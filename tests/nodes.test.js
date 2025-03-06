19cdc6a442a04cfb3fb56853062cba7730a95bb2
Srdjan S <stankovic.srdjo@gmail.com>
2025-03-06 11:01:28 +0100

    Remove redundant test

--- tests/nodes.test.js
+++ tests/nodes.test.js
@@ -19,35 +19,6 @@ test('should arweave node by default', async () => {
   }])
 })
 
-test('should return nodes with a custom fetch function', async () => {
-  const mockFetch = (pages) => {
-    return Promise.resolve({
-      json: () => ({
-        pages: 1,
-        docs: [
-          { id: '127.0.0.1:1984' },
-          { id: '127.0.0.1:1986' },
-          { id: '127.0.0.1' },
-          { id: 'test.arweave.net' },
-          { id: '' },
-          {}
-        ]
-      })
-    })
-  }
-
-  const nodes = await getNodes(mockFetch);
-  assertEquals(nodes.length, 5)
-  assertEquals(nodes, [
-    { host: 'arweave.net', port: 443, protocol: 'https' },
-    { host: '127.0.0.1', port: 1984, protocol: 'http' },
-    { host: '127.0.0.1', port: 1986, protocol: 'http' },
-    { host: '127.0.0.1', port: 80, protocol: 'http' },
-    { host: 'test.arweave.net', port: 443, protocol: 'https' },
-  ])
-})
-
-
 test('should return nodes from all pages with a custom fetch function', async () => {
   const mockFetch = (page) => {
     if (page === 0) {
